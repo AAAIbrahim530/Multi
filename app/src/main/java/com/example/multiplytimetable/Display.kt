e44d83dfd892d5d8a9619b19430072e02d331bba
AAAIbrahim530 <07attiaibrahim@gmail.com>
2026-04-01 16:54:57 +0200

    Recieved the intent data on the display page

--- app/src/main/java/com/example/multiplytimetable/Display.kt
+++ app/src/main/java/com/example/multiplytimetable/Display.kt
@@ -1,6 +1,7 @@
 package com.example.multiplytimetable
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,17 @@ class Display : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_display)
+
+        val bundle: Bundle? = intent.extras
+        val tableString: String? = bundle?.getString("numberInput")
+        val numberInput = tableString?.toInt()
+        val multiplyTable = findViewById<TextView>(R.id.textView)
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
