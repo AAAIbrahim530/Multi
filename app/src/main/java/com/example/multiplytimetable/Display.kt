ee3ca107e67737d15f6624831fb379c41bfd638a
AAAIbrahim530 <07attiaibrahim@gmail.com>
2026-04-01 16:06:37 +0200

    edit display activity

--- app/src/main/java/com/example/multiplytimetable/Display.kt
+++ app/src/main/java/com/example/multiplytimetable/Display.kt
@@ -1,25 +1,16 @@
 package com.example.multiplytimetable
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Display : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        
-        val button = findViewById<Button>(R.id.multi)
-        button.setOnClickListener {
-            val intent = Intent(this, Display::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_display)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
